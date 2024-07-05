784441d46492c304baa1cf15a4272fb53e1f8bee
202201617 <113917440+202201617@users.noreply.github.com>
2024-07-05 17:32:31 +0900

    Delete GroupProject directory

--- GroupProject/src/main/java/com/project/GroupProject/Repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.GroupProject.Repository;
-
-import com.project.GroupProject.Domain.User.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<User, Long> {
-}
